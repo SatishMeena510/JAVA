7c80c52f8c6d96ec3603b65e7bf2ba07e795b835
Satish Meena <146447607+SatishMeena510@users.noreply.github.com>
2023-12-12 22:46:42 +0530

    Create Arrays1.java

---
+++ Arrays1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Arrays1 {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner (System.in);
+        int size = sc.nextInt();
+        int num []= new int [size];
+
+      for ( int i=0; i<size; i++){
+          num[i]=sc.nextInt();
+        }
+
+        for( int i=0; i<size; i++){
+            
+            System.out.println(num[i]);
+        }
+    }
+}
